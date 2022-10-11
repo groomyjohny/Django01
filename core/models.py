bbbd571d7ee3b785ea72fdd9cbe4d7e6b452c7f3
groomyjohny <groomyjohny@gmail.com>
2022-10-12 01:19:14 +0500

    Added person model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField('Имя', max_length=255)
+    phone = models.ImageField('Номер телефона')
