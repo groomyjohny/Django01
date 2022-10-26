81191edd20cab0ecfaee43b6175330d4eaa894fa
groomyjohny <groomyjohny@gmail.com>
2022-10-26 14:09:12 +0500

    Added ProcessRecord model

--- core/models.py
+++ core/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField('Имя', max_length=255)
     phone = models.IntegerField('Номер телефона')
+
+class ProcessRecord(models.Model):
+    imagePath = models.CharField('Путь к файлу', max_length=512)
+    osPid = models.IntegerField('PID')
+    timeBegin = models.DateTimeField('Время начала')
+    timeEnd = models.DateTimeField('Время конца')
+    cpuCycles = models.BigIntegerField("Циклы процессора")
