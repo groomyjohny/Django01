cb5db6bc3e1f3ee743f429bd135efd4649803974
groomyjohny <groomyjohny@gmail.com>
2022-10-26 14:41:57 +0500

    Added admin for ProcessRecord and renamed its rows

--- core/admin.py
+++ core/admin.py
@@ -10,3 +10,13 @@ class Person(admin.ModelAdmin):
     search_fields = ('name', )
     def __str__(self):
         return self.name
+
+
+@admin.register(core.models.ProcessRecord)
+class ProcessRecord(admin.ModelAdmin):
+    list_display = ('imagePath', 'osPid', 'timestampBegin', 'timestampEnd', 'cpuTimeNs')
+    search_fields = ('imagePath', 'osPid', 'timestampBegin', 'timestampEnd')
+
+    #def __str__(self):
+    #Todo: add nice str to ProcessRecord
+    #    return [ self.imagePath, self.osPid, self.timesta
--- core/models.py
+++ core/models.py
@@ -8,6 +8,6 @@ class Person(models.Model):
 class ProcessRecord(models.Model):
     imagePath = models.CharField('Путь к файлу', max_length=512)
     osPid = models.IntegerField('PID')
-    timeBegin = models.DateTimeField('Время начала')
-    timeEnd = models.DateTimeField('Время конца')
-    cpuCycles = models.BigIntegerField("Циклы процессора")
+    timestampBegin = models.DateTimeField('Время начала')
+    timestampEnd = models.DateTimeField('Время конца')
+    cpuTimeNs = models.BigIntegerField("Процессорное время (нс)", null=True)
