3637a44b7a04a2b6406be81701d0b31f427a1a8e
groomyjohny <groomyjohny@gmail.com>
2022-10-27 10:05:12 +0500

    Updated view methods for new Record structure, added jsonToRecord, disabled CSRF in debug mode for allRecords, added image path filters

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 import json
 
+import django.conf
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from core.models import *
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
@@ -11,7 +13,23 @@ def index(request):
 def recordToJson(rec):
     return {
         'id': rec.id,
+        'imageName': rec.getImageName(),
         'imagePath': rec.imagePath,
+        'osPid': rec.osPid,
+        'osParentPid': rec.osParentPid,
+        'timestampBegin': rec.timestampBegin,
+        'timestampEnd': rec.timestampEnd,
+        'cpuTimeNs': rec.cpuTimeNs,
+    }
+
+def jsonToRecord(js):
+    rec = json.loads(js)
+    return {
+        'id': rec.id,
+        'imageName': rec.getImageName(),
+        'imagePath': rec.imagePath,
+        'osPid': rec.osPid,
+        'osParentPid': rec.osParentPid,
         'timestampBegin': rec.timestampBegin,
         'timestampEnd': rec.timestampEnd,
         'cpuTimeNs': rec.cpuTimeNs,
@@ -20,11 +38,24 @@ def recordToJson(rec):
 def querySetToJson(qs):
     arr = [recordToJson(rec) for rec in qs]
     return arr
-def allRecords(request):
+
+#view for records/
+#GET: get all available records
+#POST: expects an array of JSON records and inserts them to DB
+if django.conf.settings.DEBUG:
+    @csrf_exempt
+    def allRecords(request):
+        return allRecordsInner(request)
+else:
+    def allRecords(request):
+        return allRecordsInner(request)
+
+def allRecordsInner(request):
     if request.method == "GET":
         qs = ProcessRecord.objects.all()
         return JsonResponse(querySetToJson(qs), safe=False)
     if request.method == "POST":
+        #arr = json.loads()
         return JsonResponse() #TODO: add DB insertion
 
 def singleRecord(request, id):
@@ -43,4 +74,9 @@ def filterRecords(request):
     if 'timestampRange' in request.GET:
         args = request.GET['timestampRange'].split(',')
         qs = qs.filter(timestampBegin__lte=args[1], timestampEnd__gte=args[0])
+    if 'imagePathContains' in request.GET:
+        qs = qs.filter(imagePath__contains=request.GET['imagePathContains'])
+    if 'imagePathExact' in request.GET:
+        qs = qs.filter(imagePath=request.GET['imagePathExact'])
+
     return JsonResponse(querySetToJson(qs), safe=False)
